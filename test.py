6b6beca5045cd2a2daebfe8deb5dfa5fdd6a6f4b
Angel Chen <angelbill5914@gmail.com>
2022-04-07 15:02:15 -0400

    Fixed git ignore

--- test.py
+++
@@ -1,9 +0,0 @@
-from fsm.msi_transient_fsm import MSI_Transient_FSM
-
-def main():
-    print("Hi")
-    msiFSM = MSI_Transient_FSM("5", "Invalid", "0")
-    msiFSM.updateState("GET_S")
-
-if __name__ == "__main__":
-    main()
